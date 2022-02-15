95da04d6a340e5b3725d2307fd0760481af96141
Ilya <ilya.ilyin5@gmail.com>
2022-02-15 19:24:13 +0200

    file calc_for_tests.py init

---
+++ calc_for_tests.py
@@ -0,0 +1,24 @@
+def calculator(expression):
+    allowed = '+-/*'
+    if not any(sign in expression for sign in allowed):
+        raise ValueError(f'Выражение должно содержать хотябы один знак {allowed}')
+    for sign in allowed:
+        if sign in expression:
+            try:
+                left, right = expression.split(sign)
+                left = int(left)
+                right = int(right)
+                if sign == '+':
+                    return left + right
+                if sign == '-':
+                    return left - right
+                if sign == '*':
+                    return left * right
+                if sign == '/':
+                    return left / right
+            except(ValueError, TypeError):
+                raise ValueError('Выражение должно содержать 2 целых числа и один знак')
+
+
+if __name__ := '__main__':
+    print(calculator('102'))
