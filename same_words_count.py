5112ab8a903871cad11135351fdb0130a7551227
Ilya <ilya.ilyin5@gmail.com>
2022-09-04 19:15:37 +0300

    some optimisation

--- same_words_count.py
+++ same_words_count.py
@@ -1,42 +1,52 @@
 #  Программа подсчитает каких слов больше всего во введённом тексте
+
+import time
+start_time = time.time()
 import pyperclip
 import string
 
 print('Убедитесь, что заранее скопировали текст в буфер обмена')
 s = pyperclip.paste()
 
-s = s.replace('\n', ' ')  # Удалить знаки переноса строк, которые в Python пишется как '\n' и '\r'
-s = s.replace('\r', ' ')  # Удалить знаки переноса строк, которые в Python пишется как '\n' и '\r'
-
+#s = s.replace('\n', ' ')  # Удалить знаки переноса строк, которые в Python пишется как '\n' и '\r'
+#s = s.replace('\r', ' ')  # Удалить знаки переноса строк, которые в Python пишется как '\n' и '\r'
+s = s.replace("\r", " ").replace("\n", " ")
 
 def marks_remove(some_string: str) -> str:  # Удалим знаки препинания
-    # punctuation = string.punctuation
-    punctuation = ['.', ',', ';', ':', '!', '?', '"', "'", '(', ')', '...', '—', '«', '»', '.', ' - ']
+    punctuation = string.punctuation
+    #punctuation = ['.', ',', ';', ':', '!', '?', '"', "'", '(', ')', '...', '—', '«', '»', '.', ' - ']
     return "".join(elements for elements in some_string if elements not in punctuation)
 
 
 new_s = marks_remove(s)
 new_s = new_s.lower()
 
-mas = new_s.split(' ')  # split - метод, который разбивает сроку и делает из нее новый список
-for x in mas:  # лишние пробелы превратились в '', удалим их:
-    if '' in mas:
-        mas.remove('')
+mas1 = new_s.split(' ')  # split - метод, который разбивает сроку и делает из нее новый список
+# for x in mas:  # лишние пробелы превратились в '', удалим их:
+#     if '' in mas:
+#         mas.remove('')
+mas = [value for value in mas1 if value !='']
+
 
 unique = set(mas)  # set - метод создания множества. Мы превращаем список в множество. Множества не содержат дублей.
 
-raw_dic = {}  # создадим и заполним словарь, ключ - это слово, значение - число повторений слова в списке.
-for word in unique:
-    raw_dic[word] = int(mas.count(word))
+# raw_dic = {}  # создадим и заполним словарь, ключ - это слово, значение - число повторений слова в списке.
+# for word in unique:
+#     raw_dic[word] = int(mas.count(word))
 
-sorted_dict = {}  # создадим новый словарь
+raw_dic = {word : int(mas.count(word)) for word in unique}
+
+#sorted_dict = {}  # создадим новый словарь
 ''' Сортировка:  Тут с помощью параметра "key" - указываем как именно осуществлять сортировку
 И мы сортируем по значениям (метод .get возвращает значение ключа). Тоесть, значение - у нас кол-во слов,
 по нему и сортируем.  reverse - перевернуть'''
 sorted_keys = sorted(raw_dic, key=raw_dic.get, reverse=True)
 
-for k in sorted_keys:  # заполним новый словарь: ключ - отсортированные ключи = значение (число) по ключу из первого словаря.
-    sorted_dict[k] = raw_dic[k]
+#for k in sorted_keys:  # заполним новый словарь: ключ - отсортированные ключи = значение (число) по ключу из первого словаря.
+#    sorted_dict[k] = raw_dic[k]
+
+sorted_dict ={k: raw_dic[k] for k in sorted_keys }
+
 
 print(f'Всего слов: {len(mas)}')
 print(f'Уникальных слов: {len(unique)}')
@@ -49,6 +59,7 @@ for key, value in sorted_dict.items():
         list_one_word.append(key)  # добавим в цикле в список
 
 print(f'Не повторяющиеся слова: {", ".join(list_one_word)}')
+print("--- %s seconds ---" % (time.time() - start_time))
 
 # код, который давался без объяснений новичкам
 
